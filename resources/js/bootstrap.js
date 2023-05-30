414a499be79da5f926c972bdf9b689a5b04202f7
islamsamy214 <islamsamy214@gmail.com>
2023-05-30 16:44:23 +0300

    add new screenshot

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,3 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.withCredentials = true;
-
-//noty
-window.Noty = require("noty");
