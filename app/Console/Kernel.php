1eba343a95d277e28d60b45e658ed511493bfedf
islamsamy214 <islamsamy214@gmail.com>
2023-06-18 11:47:36 +0300

    add daily backups

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,15 +14,33 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
-    }
+        // taking backup of database every day and deleting old backups
+        // just run php artisan schedule:work
+        $schedule->call(function () {
+            Artisan::call('backup:run --only-db');
+            $this->deleteOldBackups();
+        })->daily();
+    } // end of schedule
+
+    private function deleteOldBackups()
+    {
+        $backups = Storage::disk('local')->allFiles(env('APP_NAME'));
+        // delete all backups except the last two
+        if (count($backups) > 2) {
+            foreach ($backups as $key => $backup) {
+                if ($key < count($backups) - 2) {
+                    Storage::disk('local')->delete($backup);
+                }
+            }
+        }
+    } // end of deleteOldBackups
 
     /**
      * Register the commands for the application.
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
